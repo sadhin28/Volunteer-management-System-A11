1a788a4ec5d7c78d548b891c72e5e6a8844aaa90
sadhin28 <taosifsadhin113@gmail.com>
2025-10-15 00:34:25 +0600

    add nav bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
-
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Link } from "react-router-dom";
 const NavigationBar = () => {
+    const {user,logOut}=useContext(AuthContext)
+    const signOut=()=>{
+       logOut()
+    }   
     return (
-        <div>
-            <h1>Navigation Bar</h1>
-        </div>
+    <Navbar className="bg-gray-400/50 mx-auto" fluid rounded>
+      <NavbarBrand href="https://flowbite-react.com">
+        
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Volunteer Hub</span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img={user && user.photoURL} rounded />
+          }
+        >
+          <DropdownHeader>
+            <span className="block text-sm">{user && user.displayName}</span>
+            <span className="block truncate text-sm font-medium">{user && user.email}</span>
+          </DropdownHeader>
+          <DropdownItem>
+            <Link>Add Volunteer need Post</Link>
+          </DropdownItem>
+          <DropdownItem>
+            <Link>Manage My Posts</Link>
+            </DropdownItem>
+          <DropdownDivider />
+          <DropdownItem onCanPlay={signOut}>Sign out</DropdownItem>
+        </Dropdown>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink href="#"> All volunteer Need posts</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/router.jsx'
 import AuthProvider from './Provider/AuthProvider.jsx'
-
+import 'flowbite';
+import 'flowbite-react';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
