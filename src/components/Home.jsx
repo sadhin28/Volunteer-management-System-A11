35e80e882c4c78860722d02b0a1469e4e7091b07
sadhin28 <taosifsadhin113@gmail.com>
2025-10-16 01:31:40 +0600

    almost all route is setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import useTitle from '../hooks/useTitle';
-
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 const Home = () => {
     useTitle("Home")
     return (
         <div>
-            <h1>Home</h1>
+           
+           
         </div>
     );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import flowbiteReact from "flowbite-react/plugin/vite";
-
-// https://vite.dev/config/
+import path from 'path'
 export default defineConfig({
   plugins: [react(), flowbiteReact()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
