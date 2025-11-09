018098c31c5745b956be84305358175a218ad09a
sadhin28 <taosifsadhin113@gmail.com>
2025-11-10 00:43:09 +0600

    add apply now buttoon

--- src/components/ApplyNow.jsx
+++ src/components/ApplyNow.jsx
@@ -232,7 +232,7 @@ const ApplyNow = () => {
           type="submit"
           className="text-white bg-[#62299CFF] hover:bg-[#4f1f7d] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-6 py-2.5"
         >
-          Submit
+          Apply Now
         </button>
       </form>
     </div>
