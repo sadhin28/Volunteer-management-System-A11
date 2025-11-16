a535f6d2bc0aacdafe9bef210de188ac0845470e
sadhin28 <taosifsadhin113@gmail.com>
2025-11-16 19:32:27 +0600

    add navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -49,7 +49,7 @@ const NavigationBar = () => {
       <div
         className={`fixed top-0 left-0 z-50 w-full transition-all duration-500 ${scrolled
             ? "bg-white/20 backdrop-blur-md shadow-md"
-            : "bg-gray-900/10  backdrop-blur-0"
+            : " bg-black/10 backdrop-blur-0"
           }`}
       >
         {/* 🌈 Center-aligned container for navbar content */}
