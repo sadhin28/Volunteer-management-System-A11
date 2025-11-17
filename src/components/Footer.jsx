d5ef16b86b7f2269e7a507e544810b91cfb71b33
sadhin28 <taosifsadhin113@gmail.com>
2025-11-17 20:39:47 +0600

    add footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,13 +18,13 @@ const Footer = () => {
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white ">Follow us</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a target='_blank' href="https://github.com/sadhin28" class="hover:underline ">Github</a>
+                          <a target='_blank' href="https://github.com/sadhin28" class="hover:underline hover:text-[#511AB7FF]">Github</a>
                       </li>
                       <li class="mb-4">
-                          <a target='_blank' href="https://sadhin-dev-com.vercel.app" class="hover:underline ">Protfolio</a>
+                          <a target='_blank' href="https://sadhin-dev-com.vercel.app" class="hover:underline hover:text-[#511AB7FF]">Protfolio</a>
                       </li>
                       
                   </ul>
@@ -34,10 +34,10 @@ const Footer = () => {
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
                           
-                          <Link to='/privacy-policy' class="hover:underline">Privacy Policy</Link>
+                          <Link to='/privacy-policy' class="hover:underline hover:text-[#511AB7FF]">Privacy Policy</Link>
                       </li>
                       <li>
-                          <Link to='/terms-and-conditions' class="hover:underline">Terms &amp; Conditions</Link>
+                          <Link to='/terms-and-conditions' class="hover:underline hover:text-[#511AB7FF]">Terms &amp; Conditions</Link>
                       </li>
                   </ul>
               </div>
