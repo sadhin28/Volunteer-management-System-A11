b0fdf685854a79f03c4bcd2345497052dbebd377
sadhin28 <taosifsadhin113@gmail.com>
2025-11-18 20:36:09 +0600

    add modal

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -90,7 +90,7 @@ const Profile = () => {
                     >
                         {/* Modal Content */}
                         <div
-                            className="bg-gradient-to-b from-[#300d81]  rounded-xl p-4 relative"
+                            className="bg-gradient-to-b from-[#6539cd]  rounded-xl p-4 relative"
                             onClick={(e) => e.stopPropagation()} // Prevent close on image click
                         >
                             <button
